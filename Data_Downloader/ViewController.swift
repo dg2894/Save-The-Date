9f0a2fdf46fbed2c367a41cfcb0ea330c42be745
Danielle Gray <dg2894@g.rit.edu>
2015-10-14 11:12:25 -0400

    Initial Commit

---
+++ Data_Downloader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Data_Downloader
+//
+//  Created by Danielle Gray on 10/14/15.
+//  Copyright © 2015 DanielleGray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

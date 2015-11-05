695617bcca1d8a696af8c2d4ef54853858cb3779
kurtweinstein <kurtweinstein@yahoo.com>
2015-11-05 11:40:32 -0500

    YAY
    Added in the tab bar controller, some icons, and changed around a
    little code because I’m cool

--- Data_Downloader/ViewController.swift
+++ Data_Downloader/ViewController.swift
@@ -18,8 +18,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
     
     func searchEvents(location:String){
